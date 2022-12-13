5a81946a85043471df221a38a09c100cbd502d0b
Michael Davis <micdavis93@gmail.com>
2022-12-12 17:23:44 -0700

    commit tree 31b05bb58e99461a4cdd6c364e8482f534d0502e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
-def countdown
+
+def countdown(sec=3)
+    while sec > 0
+        puts "#{sec} SECOND(S)!"
+        sec -= 1
+
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(sec=3)
+    while sec > 0
+        puts "#{sec} SECOND(S)!"
+        sec -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
 end
